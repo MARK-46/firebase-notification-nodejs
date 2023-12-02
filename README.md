ef9fb74a3ea541da730bf4abb1e3a82f15d132a0
MARK-46 <mark.38.98.ii@gmail.com>
2023-12-02 23:19:41 +0400

    fcm

---
+++ README.md
@@ -0,0 +1,6 @@
+### Install & Run
+```ps1
+npm install
+node .\main.js
+curl --location --request GET 'http://localhost:1337/api/notify?title=NotificationTitle&body=NotificationBody&token=DeviceToken'
+```
---
+++ main.js
@@ -0,0 +1,76 @@
+const request = require("request");
+const firebase = require("firebase-admin");
+
+const account = require("./service-account.json");
+const fcm = firebase.initializeApp({
+    credential: firebase.credential.cert(account),
+    projectId: account.project_id,
+});
+
+const app = require('express')();
+app.get('/api/notify', async (req, res) => {
+    try {
+        fcm.auth().app.INTERNAL.getToken().then(token => {
+            const url = `https://fcm.googleapis.com/v1/projects/${account.project_id}/messages:send`;
+            const options = {
+                url: url,
+                method: 'post',
+                headers: {
+                    'Authorization': `Bearer ${token.accessToken}`,
+                    'X-Firebase-Client': 'fire-admin-node/11.11.1',
+                    'access_token_auth': 'true',
+                    'content-type': 'application/json',
+                },
+                json: {
+                    "message": {
+                        "token": req.query.token,
+                        "notification": {
+                            "title": "Incoming call",
+                            "body": req.query.body
+                        },
+                        "data": {
+
+                        },
+                        "fcmOptions": {
+                            "analyticsLabel": "Incoming call"
+                        },
+                        "android": {
+                            "priority": "high"
+                        },
+                        "apns": {
+                            "payload": {
+                                "aps": {
+                                    "priority": 'high',
+                                    "timeToLive": 3600,
+                                    "contentAvailable": true,
+                                    "mutableContent": true
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            request(url, options,function (error, response, body) {
+                const opt = Object.freeze(options);
+                opt.headers.Authorization = opt.headers.Authorization.length;
+                res.json({
+                    options: options,
+                    error: error,
+                    headers: response.headers,
+                    status_code: response.statusCode,
+                    body: body === response.body ? body : {
+                        body1: body,
+                        body2: response.body,
+                    },
+                });
+            });
+        });
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+const port = 1337;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${ port }/api/notify?title=Incoming%20call&body=Test&token=...`);
+});
